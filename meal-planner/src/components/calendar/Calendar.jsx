e5f26eebf7fe5f2f57052aac73ca87feefd17bfc
Jared Stoddard <jared.stoddard2@gmail.com>
2024-10-20 16:26:51 -0700

    added way of displaying amount of days a meal that has been added to calendar will last using serving sizes

--- meal-planner/src/components/calendar/Calendar.jsx
+++ meal-planner/src/components/calendar/Calendar.jsx
@@ -2,24 +2,25 @@ import "./Calendar.css";
 import { useState, useRef } from "react";
 import xIcon from "../../assets/x.svg";
 export default function Calendar({ mealOptions, draggedValueRef }) {
-  const [addedMeals, setAddedMeals] = useState({
-    1: [
-      {
-        name: "Burger",
-        link: "",
-        servings: 4,
-        prepTimeMin: 25,
-        ingredients: ["onions", "buns", "patty"],
-      },
-      {
-        name: "tortilla wrap",
-        link: "",
-        servings: 2,
-        prepTimeMin: 10,
-        ingredients: ["tortilla", "meat"],
-      },
-    ],
-  });
+  const [addedMeals, setAddedMeals] = useState([
+    // {
+    //   name: "Burger",
+    //   link: "",
+    //   servings: 2,
+    //   prepTimeMin: 25,
+    //   ingredients: ["onions", "buns", "patty"],
+    //   assignment: 1,
+    // },
+    // {
+    //   name: "tortilla wrap",
+    //   link: "",
+    //   servings: 2,
+    //   prepTimeMin: 10,
+    //   ingredients: ["tortilla", "meat"],
+    //   assignment: 3,
+    // },
+  ]);
+  console.log("addedMeals", addedMeals);
 
   const days = [
     "Monday",
@@ -60,6 +61,21 @@ function Day({
   addedMeals,
   mealOptions,
 }) {
+  const colorOptions = [
+    "#264653", // Deep Blue Teal
+    "#2A9D8F", // Dark Aqua
+    "#E76F51", // Warm Terracotta
+    "#F4A261", // Soft Amber
+    "#E9C46A", // Golden Yellow
+    "#1D3557", // Midnight Blue
+    "#457B9D", // Slate Blue
+    "#8D99AE", // Gentle Slate
+    "#6A4C93", // Rich Purple
+    "#FF6F61", // Vibrant Coral
+    "#D62828", // Fiery Red
+    "#264653", // Deep Blue Green
+  ];
+
   const targetRef = useRef(null); // Ref to directly manipulate the DOM content
   const date = new Date();
   const currentDay = date.getDay();
@@ -70,33 +86,39 @@ function Day({
 
   const handleDrop = (e) => {
     e.preventDefault();
+    const index = e.currentTarget.id;
     if (targetRef.current) {
       // targetRef.current.innerText = draggedValueRef.current; // Set the content of the drop target to the dragged value
       setAddedMeals((prev) => {
-        if (prev[dayIndex]) {
-          return {
-            ...prev,
-            [dayIndex]: [
-              ...prev[dayIndex],
-              mealOptions[draggedValueRef.current.id],
-            ],
-          };
-        } else {
-          return {
-            ...prev,
-            [dayIndex]: [mealOptions[draggedValueRef.current.id]],
-          };
-        }
+        // if (prev[dayIndex]) {
+        return [
+          ...prev,
+          {
+            ...mealOptions[draggedValueRef.current.id],
+            assignment: index,
+          },
+        ];
+        // } else {
+        //   return [
+        //     {
+        //       ...mealOptions[draggedValueRef.current.id],
+        //       assignment: index,
+        //     },
+        //   ];
+        // }
       });
     }
   };
   function handleRemoveMeal(e) {
     const index = e.currentTarget.id;
     setAddedMeals((prev) => {
-      const copy = [...prev[dayIndex]];
+      const copy = [...prev];
       copy.splice(index, 1);
-      return { ...prev, [dayIndex]: copy };
+      return copy;
     });
+  }
+  function getColor(index) {
+    return colorOptions[+index];
   }
 
   return (
@@ -110,22 +132,33 @@ function Day({
         {day}
       </div>
       <div className="added-meals-list">
-        {!addedMeals[dayIndex] || addedMeals[dayIndex].length == 0 ? (
-          <div className="placeholder-text">Empty</div>
-        ) : (
-          <>
-            {addedMeals[dayIndex].map((meal, index) => {
+        <>
+          {addedMeals.map((meal, index) => {
+            const assignment = meal.assignment;
+            console.log("colorOptions[+meal.assignment]");
+            console.log(meal.assignment);
+            if (
+              dayIndex == assignment ||
+              (dayIndex >= assignment &&
+                dayIndex < +assignment + +meal.servings)
+            ) {
               return (
-                <div key={index} className="meal-item-container">
+                <div
+                  style={{
+                    backgroundColor: `${getColor(index)}`,
+                  }}
+                  key={index}
+                  className="meal-item-container"
+                >
                   <div>{meal.name}</div>
                   <button id={index} onClick={handleRemoveMeal}>
                     <img src={xIcon} />
                   </button>
                 </div>
               );
-            })}
-          </>
-        )}
+            }
+          })}
+        </>
       </div>
     </div>
   );
